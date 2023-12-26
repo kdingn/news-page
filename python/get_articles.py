77147d076fba3b31e3d7cce6951142660bfc24b4
kdingn <koukou3335@gmail.com>
2023-12-27 02:58:42 +0900

    python file corrected

--- python/get_articles.py
+++ python/get_articles.py
@@ -1,12 +1,23 @@
+import time
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+def get_ogimage(df):
+    print(f"{len(df)} new articles")
+    images = []
+    for link in df["link"]:
+        soup = BeautifulSoup(requests.get(link).content, features="html.parser")
+        thumb = soup.find('meta', attrs={'property': 'og:image'}).get("content")
+        images.append(thumb)
+        time.sleep(1)
+    df["ogimage"] = images
+    return df
 
-def get_aidb():
+
+def get_aidb(df_org):
     url = "https://aiboom.net"
-    res = requests.get(url)
-    soup = BeautifulSoup(res.content, features="html.parser")
+    soup = BeautifulSoup(requests.get(url).content, features="html.parser")
     article = list(
         map(
             lambda x: {
@@ -20,13 +31,16 @@ def get_aidb():
     df = pd.DataFrame(article).sort_values(["date", "link"])
     df["date"] = pd.to_datetime(df["date"])
     df["source"] = "aidb"
+    df = df[df["link"].apply(lambda x: x not in df_org["link"])]
+    df = get_ogimage(df)
     return df
 
 
 def main():
+aa
     df = pd.read_csv("src/assets/articles.csv")
     df["date"] = pd.to_datetime(df["date"])
-    df = pd.concat([df, get_aidb()])
+    df = pd.concat([df, get_aidb(df)])
     df = (
         df.sort_values("date", ascending=False)
         .drop_duplicates("link", keep="first")
