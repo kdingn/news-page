b33451db6fcbbd70aa34401adabbc3331666cb53
kdingn <koukou3335@gmail.com>
2024-01-03 03:28:00 +0900

    create sidebar contents

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
 import Card from "./components/Card.vue";
 import Header from "./components/Header.vue";
+import Sidebar from "./components/Sidebar.vue";
 
 import wholeArticles from "./assets/articles.csv";
 let articles = wholeArticles.slice(0, 60);
 
 import "./styles/common.css";
-if (/Android|iPhone/i.test(navigator.userAgent)) {
-  import("./styles/spStyle.css");
-} else {
+const pcMode = !/Android|iPhone/i.test(navigator.userAgent);
+if (pcMode) {
   import("./styles/pcStyle.css");
+} else {
+  import("./styles/spStyle.css");
 }
 </script>
 
 <template>
   <div class="background">
     <Header />
+    <div class="sidebar" v-if="pcMode">
+      <Sidebar />
+    </div>
     <div class="contents">
       <div v-for="article in articles">
         <Card :article="article" />
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import sources from "../assets/sources.csv";
+</script>
+
+<template>
+  <div v-for="source in sources" class="sidebar-page">
+    <hr />
+    <a :href="source.source" target="_blank" class="sidebar-page-link">
+      {{ source.title }}<img :src="source.ogimage" class="sidebar-page-image"
+    /></a>
+  </div>
+  <hr />
+</template>
