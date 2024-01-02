6f3528f2e89d66935fa0ca0d12f07099d778c33d
kdingn <koukou3335@gmail.com>
2024-01-02 12:13:50 +0900

    check smartphone

--- src/stores/device.ts
+++ src/stores/device.ts
@@ -1,11 +1,19 @@
 import { defineStore } from "pinia";
 
 function isSmartPhone() {
-  if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(max-device-width: 640px)").matches
+  ) {
     return true;
   } else {
     return false;
   }
+  // if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
 
 export const useDeviceStore = defineStore("device", () => {
