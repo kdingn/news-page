a5a0c9572f86f83183f25e4113658af719d030ea
kdingn <koukou3335@gmail.com>
2024-01-04 02:57:22 +0900

    layout changed

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@ import Card from "./components/Card.vue";
 import Header from "./components/Header.vue";
 import Sidebar from "./components/Sidebar.vue";
 
-import wholeArticles from "./assets/articles.csv";
-let articles = wholeArticles.slice(0, 60);
-
 import "./styles/common.css";
 const pcMode = !/Android|iPhone/i.test(navigator.userAgent);
 if (pcMode) {
@@ -15,6 +12,23 @@ if (pcMode) {
   const style = import("./styles/spStyle.css");
   new Promise((resolve) => resolve(style));
 }
+
+import wholeArticles from "./assets/articles.csv";
+const articles = wholeArticles.slice(0, 20);
+const recArticles: { [k: string]: string }[] = [];
+const othArticles: { [k: string]: string }[] = [];
+const recday = new Date();
+recday.setDate(recday.getDate() - 7);
+articles.map((x: { [k: string]: string }) => {
+  if (new Date(x.date) > recday) {
+    recArticles.push(x);
+  } else {
+    othArticles.push(x);
+  }
+});
+
+const recTitle = "— Recent News —";
+const othTitle = "— Archive —";
 </script>
 
 <template>
@@ -24,8 +38,24 @@ if (pcMode) {
       <Sidebar />
     </div>
     <div class="contents">
-      <div v-for="article in articles">
-        <Card :article="article" />
+      <div class="contents-title" v-if="pcMode">
+        <span class="contents-title-shadow"> {{ recTitle }}</span>
+      </div>
+      <div class="contents-rec-cards">
+        <div v-for="article in recArticles">
+          <Card :article="article" />
+        </div>
+      </div>
+      <div class="contents-hr" v-if="pcMode"><hr color="black" size="1" /></div>
+      <div class="contents-title" v-if="pcMode">
+        <span class="contents-title-shadow">
+          {{ othTitle }}
+        </span>
+      </div>
+      <div class="contents-rec-cards">
+        <div v-for="article in othArticles">
+          <Card :article="article" />
+        </div>
       </div>
     </div>
   </div>
